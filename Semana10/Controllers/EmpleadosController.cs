76c97f47d1b8d0063b2e86a22dc8348556f3a15a
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2024-05-25 15:49:58 -0500

    Agregar archivos de proyecto.

---
+++ Semana10/Controllers/EmpleadosController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Semana10.Models;
+
+namespace Semana10.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private NeptunoDBEntities db = new NeptunoDBEntities();
+
+        // GET: Empleados
+        //public ActionResult Index()
+        //{
+        //    return View(db.Empleados.ToList());
+        //}
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            // Filtra los empleados para mostrar solo aquellos con Estado = 1
+            var empleadosActivos = db.Empleados.Where(e => e.estado == 1).ToList();
+            return View(empleadosActivos);
+        }
+
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "IdEmpleado,Apellidos,Nombre,cargo,Tratamiento,FechaNacimiento,FechaContratacion,direccion,ciudad,region,codPostal,pais,TelDomicilio,Extension,notas,Jefe,sueldoBasico,estado")] Empleados empleados)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Empleados.Add(empleados);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(empleados);
+        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdEmpleado,Apellidos,Nombre,cargo,Tratamiento,FechaNacimiento,FechaContratacion,direccion,ciudad,region,codPostal,pais,TelDomicilio,Extension,notas,Jefe,sueldoBasico,estado")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                // Establecer el valor del campo estado a 1
+                empleados.estado = 1;
+
+                db.Empleados.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empleados);
+        }
+
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdEmpleado,Apellidos,Nombre,cargo,Tratamiento,FechaNacimiento,FechaContratacion,direccion,ciudad,region,codPostal,pais,TelDomicilio,Extension,notas,Jefe,sueldoBasico,estado")] Empleados empleados)
+        {
+            // Asigna el valor 1 al campo estado
+            empleados.estado = 1;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empleados);
+        }
+
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Empleados empleados = db.Empleados.Find(id);
+        //    db.Empleados.Remove(empleados);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados != null)
+            {
+                empleados.estado = 0; // Cambiar el estado a 0 para indicar eliminación lógica
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
